8f6b722f9d00fdc2ebbbf85b1278432de76f2137
zoki <zoran.smilevski@biokoda.com>
2024-12-04 22:35:41 +0100

    *day 13 part 2

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,8 +1,17 @@
-private data class PatternData(
-    val data: CharArray
+private class PatternData(
+    val data: String,
+    val chars: CharArray = data.toCharArray()
 ) {
     fun diff(other: PatternData): Int {
-        return data.zip(other.data).count { pair -> pair.first != pair.second }
+        return chars.zip(other.chars).count { pair -> pair.first != pair.second }
+    }
+
+    private val hashNum by lazy {
+        data.replace('#', '1').replace('.', '0').toInt(2)
+    }
+
+    override fun toString(): String {
+        return hashNum.toString()
     }
 
     override fun equals(other: Any?): Boolean {
@@ -11,13 +20,11 @@ private data class PatternData(
 
         other as PatternData
 
-        if (!data.contentEquals(other.data)) return false
-
-        return true
+        return chars.contentEquals(other.chars)
     }
 
     override fun hashCode(): Int {
-        return data.contentHashCode()
+        return chars.contentHashCode()
     }
 }
 
@@ -30,7 +37,7 @@ private data class Pattern(
 
     private fun generateHashes() {
         hashH = horizontal.map { string ->
-            PatternData(string.toCharArray())
+            PatternData(string)
         }
 //        println(hashH)
 
@@ -47,57 +54,60 @@ private data class Pattern(
                                 }
                             )
                         }
-                    }.toCharArray()
+                    }
                 )
             )
         }
 //        println(hashV)
     }
 
-    private fun checkPatternData(pattern: List<PatternData>, index: Int, size: Int): Int {
+    private fun checkPatternData(pattern: List<PatternData>, index: Int, size: Int, allowDiff: Int): Int {
         val part1 = pattern.subList(index - size, index)
         val part2 = pattern.subList(index, index + size)
         val reverse = part2.reversed()
-        val isEqual = part1 == reverse
-//        println("equal=$isEqual for part1: $part1, part2: $part2 -> $reverse")
-        return when (isEqual) {
+        val diffs = part1.zip(reverse).sumOf { pair -> pair.first.diff(pair.second) }
+//        println("diffs=$diffs for part1: $part1, part2: $part2 -> $reverse")
+        return when (diffs == allowDiff) {
             true -> index
             false -> -1
         }
     }
 
-    private fun checkSubPattern(pattern: List<PatternData>, index: Int): Int {
+    private fun checkSubPattern(pattern: List<PatternData>, index: Int, allowDiff: Int): Int {
         val size = minOf(index, pattern.size - index)
-        return checkPatternData(pattern, index, size)
+        val validIndex = checkPatternData(pattern, index, size, allowDiff)
+//        println("valid index $validIndex")
+        return validIndex
     }
 
-    private fun findAllEquals(pattern: List<PatternData>): List<Int> {
+    private fun findAllEquals(pattern: List<PatternData>, allowDiff: Int): List<Int> {
         return pattern.mapIndexedNotNull { index, value ->
-            if (index != 0 && value.diff(pattern[index - 1]) == 0) index else null
+            if (index != 0 && value.diff(pattern[index - 1]) <= allowDiff) index else null
         }
     }
 
-    private fun checkPattern(pattern: List<PatternData>): Int {
-        val indexes = findAllEquals(pattern)
+    private fun checkPattern(pattern: List<PatternData>, allowDiff : Int): Int {
+        val indexes = findAllEquals(pattern, allowDiff)
 //        println("$pattern -> $indexes")
+        //firstNotNullOfOrNull
         return indexes.maxOfOrNull { index ->
-            checkSubPattern(pattern, index)
+            checkSubPattern(pattern, index, allowDiff)
         } ?: -1
     }
 
-    fun calculateResult(): Int {
+    fun calculateResult(allowDiff : Int): Int {
         generateHashes()
-        val hResult = checkPattern(hashH)
+        val hResult = checkPattern(hashH, allowDiff)
         if (hResult != -1) {
 //            println("Horizontal match $hResult")
             return hResult * 100
         }
-        val vResult = checkPattern(hashV)
+        val vResult = checkPattern(hashV, allowDiff)
         if (vResult != -1) {
 //            println("Vertical match $vResult")
             return vResult
         }
-        println("No match ($hashH $hashV)")
+//        println("No match ($hashH $hashV)")
         return 0
     }
 }
@@ -120,20 +130,27 @@ fun main() {
 
 
     fun part1(input: List<String>): Int {
+        println("Part 1")
         val patterns = parsePatterns(input)
-        return patterns.sumOf { pattern -> pattern.calculateResult() }
+        return patterns.sumOf { pattern -> pattern.calculateResult(0) }
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        println("Part 2")
+        val patterns = parsePatterns(input)
+        val sum = patterns.sumOf { pattern -> pattern.calculateResult(1) }
+//        println("Part 2 sum $sum")
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day13_test")
-    check(part1(testInput) == 405)
+//    check(part1(testInput) == 405)
+    check(part2(testInput) == 400)
 
     val input = readInput("Day13")
-    check(part1(input) == 33735)
+    val part1Res = part1(input)
+    check(part1Res == 33735)
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
